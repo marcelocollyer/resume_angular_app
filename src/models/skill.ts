c679921603585f55f646d5df9a7366c01ecd138b
Marcelo Collyer <marcelocollyer@gmail.com>
2018-06-21 07:38:11 -0300

    Adding collapse/expand behavior. Adding children skills to the model.

--- src/models/skill.ts
+++ src/models/skill.ts
@@ -2,12 +2,15 @@
 export class Skill {
   description: string;
   level: string;
+  children: Skill[]
 
   constructor(
     description: string,
-    level: string
+    level: string,
+    children: Skill[]
   ) {
     this.description = description;
     this.level = level;
+    this.children = children;
   }
-}
+} 
